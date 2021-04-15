8d2c3e1ddb65227038dfaf60a0e75f7ff701fc01
Femi Alayande <alayandefs@gmail.com>
2021-04-15 19:27:54 +0100

    Updated to include username

--- WebScraping.py
+++ WebScraping.py
@@ -1,4 +1,4 @@
-# Scraping github to extract profile picture
+# Scraping github to extract user name and profile picture
 
 from bs4 import BeautifulSoup
 import requests
@@ -11,6 +11,7 @@ data_request = requests.get(github_url)
 soup = BeautifulSoup(data_request.content, 'html.parser')
 
 profile_picture = soup.find('img', {'style': 'height:auto;'})['src']
+user_name = soup.find(class_='p-name').get_text()
 
-print(f"Follow below link to {username}'s Github profile picture")
+print(user_name)
 print(profile_picture)
